92379821158423a97128c52fe0d138053b91b9ab
Tirudeepak <deepakkasukurthi@gmail.com>
2021-04-05 18:36:22 +0530

    fits commit

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
-@login_required
+#@login_required
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
